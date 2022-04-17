42b45d02a86b0b5ddec929262a00a7608d2b65ab
Carlos Klein <c.edudepaula@yahoo.com.br>
2022-04-16 21:50:20 -0300

    001 Início

---
+++ src/main/java/com/carlosklein/pegadacarbonospring/PegadaCarbonoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.carlosklein.pegadacarbonospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegadaCarbonoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegadaCarbonoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosklein/pegadacarbonospring/PegadaCarbonoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosklein.pegadacarbonospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegadaCarbonoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
