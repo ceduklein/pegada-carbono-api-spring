081e7512fa25d2f17a7c60322bbef2f820750d07
Carlos Klein <c.edudepaula@yahoo.com.br>
2022-05-03 15:27:18 -0300

    005 Correção Services Colaborador e Veículo

--- src/main/java/com/carlosklein/service/ColaboradorService.java
+++ src/main/java/com/carlosklein/service/ColaboradorService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.carlosklein.exception.RegraNegocioException;
@@ -25,7 +26,7 @@ public class ColaboradorService {
 	}
 	
 	public List<Colaborador> list() {
-		return repository.findAll();
+		return repository.findAll(Sort.by(Sort.Direction.ASC, "id"));
 	}
 	
 	public Optional<Colaborador> findById(Long id) {
--- src/main/java/com/carlosklein/service/VeiculoService.java
+++ src/main/java/com/carlosklein/service/VeiculoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.carlosklein.exception.RegraNegocioException;
@@ -54,7 +55,7 @@ public class VeiculoService {
 	}
 	
 	public List<Veiculo> list() {
-		return repository.findAll();
+		return repository.findAll(Sort.by(Sort.Direction.ASC, "id"));
 	}
 	
 	public Optional<Veiculo> findById(Long id) {
